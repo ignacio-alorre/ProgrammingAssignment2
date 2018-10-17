de453e0ec3cdb7370fc5bef7add33f3843ce3600
ignacio-alorre <ignacio.alorre@gmail.com>
2018-10-17 11:39:50 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+#This function generates an object which contains a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  #definining the setters and getters for the function
+  set <- function(y){
+      #Assign the matrix and make null the inverse
+      x <<- y
+      inv <<- NULL
+  }
+  get <- function() x
+  #Without the <<- operator the inverse will not be saved from cacheSolved
+  setinverse <- function(inver) inv <<- inver 
+  getinverse <- function() inv 
+  #list of methods
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#This method calculates the inverse of a given matrix, in case the inverse has been already calculated
+#it returs the saved value (simulating a cache)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)){
+      message("getting cached data")
+      return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+#For testing I used this invertible matrix I found
+#myMatrix <- matrix(c(4,7,2,6), 2, 2)
+#myCacheMatrix <- makeCacheMatrix(myMatrix)
+#cacheSolve(myCacheMatrix)
+#cacheSolve(myCacheMatrix)
+#After repeating the call we get the message: "getting cached data"
